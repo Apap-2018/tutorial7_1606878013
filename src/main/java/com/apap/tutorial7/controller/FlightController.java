39d6a38464cdfc6ff054943dfbccd9ceed4aff5f
kaelky <rickychandra.j@gmail.com>
2018-11-01 19:11:09 +0700

    Exercise 2 - Update a Flight

--- src/main/java/com/apap/tutorial7/controller/FlightController.java
+++ src/main/java/com/apap/tutorial7/controller/FlightController.java
@@ -3,10 +3,9 @@ package com.apap.tutorial7.controller;
 import com.apap.tutorial7.model.FlightModel;
 import com.apap.tutorial7.service.FlightService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Date;
 
 @RestController
 @RequestMapping("/flight")
@@ -17,5 +16,19 @@ public class FlightController {
     @PostMapping(value = "/add")
     public FlightModel addFlightSubmit(@RequestBody FlightModel flight) {
         return flightService.addFlight(flight);
+    }
+
+    @PutMapping(value = "/update/{flightId}")
+    public String updateFlightSubmit(@PathVariable("flightId") long flightId,
+                                     @RequestParam("destination") String destination,
+                                     @RequestParam("origin") String origin,
+                                     @RequestParam("time") Date time) {
+        FlightModel flight = flightService.findFlightById(flightId);
+        if (flight.equals(null)){
+            return "Couldn't find your flight";
+        }
+
+        flightService.updateFlight(flight, destination, origin, time);
+        return "flight update success";
     }
 }
